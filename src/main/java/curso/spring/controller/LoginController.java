0c3ab2d8d46909b82afabdb0d8dd0f554d3fc831
Luis <luis.mangas15@gmail.com>
2021-05-05 12:44:49 +0200

    Hecho ejercicio 3, valida usuario y contraseña. Saluda.

---
+++ src/main/java/curso/spring/controller/LoginController.java
@@ -0,0 +1,41 @@
+package curso.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import curso.spring.model.Contacto;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	@GetMapping("")
+	public String altaContacto(Model model, @RequestParam String usuario, @RequestParam(defaultValue="admin") String clave) {
+		
+		if(usuario.equals("admin") && clave.equals("admin")) {
+			model.addAttribute("usuario", usuario);
+			
+//			WebClasicaController.saludo(model);
+//			
+//			return "redirect:http://localhost:9090/webclasica/saludo";
+			
+			WebClasicaController wCC = new WebClasicaController();
+			wCC.saludo(model);
+			
+			
+			return wCC.saludo(model);
+		}
+		else {
+			return "redirect:error";
+		}
+	}
+	
+	@GetMapping("/error")
+	public String error() {
+		
+		return "error";
+	}
+}
--- src/main/java/curso/spring/controller/WebClasicaController.java
+++ src/main/java/curso/spring/controller/WebClasicaController.java
@@ -1,16 +1,31 @@
 package curso.spring.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import curso.spring.model.Contacto;
 
 @Controller
 @RequestMapping("/webclasica")
 public class WebClasicaController {
 	
+	ArrayList<Contacto> listaContactos = new ArrayList<Contacto>();
+	
 	@GetMapping("")
 	public String portada() {
 		return "portada";
+	}
+	
+	@GetMapping("/saludo")
+	public String saludo(Model model) {
+		
+		return "saludo";
 	}
 	
 	@GetMapping("/que")
@@ -18,8 +33,53 @@ public class WebClasicaController {
 		return "que";
 	}
 	
+	@GetMapping("/alta")
+	public String altaContacto(Model model, @RequestParam("n") String nombre, @RequestParam(name="a", required=false, defaultValue="MOngo") String apellidos, @RequestParam String telefono, @RequestParam String email ) {
+		
+		Contacto c = new Contacto(nombre, apellidos, telefono, email);
+		
+		listaContactos.add(c);
+		
+		//Si no se redirige se le tiene que pasar el model
+		contacto(model);
+		return "contacto";
+//		return "redirect:contacto";
+	}
+	
+	@GetMapping("/alta/{nombre}/insertar")
+	public String altaContacto(Model model, @PathVariable String nombre) {
+		String apellidos = "default path";
+		String telefono = "default path2";
+		String email = "default path3";
+		Contacto c = new Contacto(nombre, apellidos, telefono, email);
+		
+		listaContactos.add(c);
+		
+		//Si no se redirige se le tiene que pasar el model
+		contacto(model);
+		return "contacto";
+		//return "redirect:contacto";
+	}
+	
 	@GetMapping("/contacto")
-	public String contacto() {
+	public String contacto(Model model) {
+		//Paso un contacto
+		model.addAttribute("contacto", new Contacto());
+		
+		//Lista de prueba
+		ArrayList<String> lista = new ArrayList<String>();
+		
+		lista.add("je");
+		lista.add("ja");
+		lista.add("ju");
+		model.addAttribute("lista", lista);
+		
+		//Lista de prueba
+		listaContactos.add(new Contacto());
+		listaContactos.add(new Contacto("Jorge","Caiceo","789789789","yorch@gemeil.com"));
+		listaContactos.add(new Contacto("Sergio","Alonso","13123132","sergo@taho.org"));
+		model.addAttribute("listaContactos", listaContactos);
+		
 		return "contacto";
 	}
 }
